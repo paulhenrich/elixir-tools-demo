d09adc97c87c9812203cf1b8f05d929967101e90
Paul Henrich <paul@henrich.me>
2016-12-06 17:39:41 -0500

    Add Sum

--- lib/demo.ex
+++ lib/demo.ex
@@ -1,2 +1,7 @@
 defmodule Demo do
+  # stolen from my "Programming Elixir" exercises
+  def sum(list), do: sum(list, 0)
+
+  defp sum([], total),             do: total
+  defp sum([ head | tail], total), do: sum(tail, head + total)
 end
